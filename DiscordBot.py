8de9245e55daab52922007c4f53cabd36ad0712b
Ahmet_Sahin <73609846+ahmet541@users.noreply.github.com>
2022-01-20 16:54:49 +0300

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -8,14 +8,16 @@ import os
 import requests
 import json
 import random
-from discord.ext import commands
+import youtube_d1
+from discord.ext import commands,tasks
 from dotenv import load_dotenv
 
 
 
 load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
-client = discord.Client()
+# client = discord.Client()  # delete?
+client = commands.Bot(command_prefix="!")
 
 words_relatedTo_Movies = ["Movie", "Movies", "movie", "movies"]
 starter_recommendedMovies = ["Edge Of Tomorrow", "The Arq", "The Tomorrows War"]
@@ -69,6 +71,63 @@ def update_recommendedMovie(new_movie):
 def delete_recommendedMovie(index):
     if index < len(starter_recommendedMovies):
         del starter_recommendedMovies[index]
+
+
+@client.command( alliases = ['p'], help = 'Play video using key word or url')
+async def play(ctx, ulr: str):
+
+    if not ctx.message.author.voice:
+        await ctx.send("{} is not connected to any voice channel.".format(ctx.message.author.name))
+        return
+    else:
+        voiceChannel = ctx.message.author.voice.channel   
+        voiceChannel.connect();      
+    
+
+
+
+    
+
+@client.command(help = 'Tells bot to join voice channel')
+async def join(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send("{} is not connected to any voice channel.".format(ctx.message.author.name))
+        return
+    else:
+        channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+@client.command(help = "Tells bot to leave from voice channel")
+async def leave(ctx):
+    bot = ctx.message.guild.voice_client
+    if bot.is_connected():
+        await bot.disconnected()
+    else:
+        await ctx.send('Bot is not in any voice channel.')
+
+@client.command(help = "Pauses bot's action")
+async def pause(ctx):
+    bot = ctx.message.guild.voice_client
+    if bot.is_playin():
+        await bot.pause()
+    else:
+        await ctx.send("Bot is not playing anyting right now.")    
+
+@client.command(help = "Resumes bot's action")
+async def resume(ctx):
+    bot = ctx.message.guild.voice_client
+    if bot.is_paused():
+        await bot.resume()
+    else:
+        await ctx.send("Bot should be paused before it to resume. Use '!play' command") 
+
+@client.command(help="Stops the bot")
+async def stop(ctx):
+    bot = ctx.message.voice_client
+    if bot.is_playing():
+        await bot.stop()
+    else:
+        await ctx.send("Bot should be playing before it to stop. Use '!play' command")
 
 
 
